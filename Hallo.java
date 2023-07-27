be6228fc7c81f421ccf55733002354f3557ddb98
uswitch-02 <chunxidubian64@gmail.com>
2023-07-27 14:02:54 +0000

    [start]1-4

---
+++ Hallo.java
@@ -0,0 +1,10 @@
+// java概要：javaは、現代のソフトウェア開発で主流となっているプログラミング言語の1つ
+// 他のプログラミング言語と違い、「一度書いたら、どこでも動くという特徴がある」
+
+// Java開発環境：ソフトウェアを開発するには、JDK（Java　Development Kit）という開発用キットが必要
+
+public class Hallo {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
